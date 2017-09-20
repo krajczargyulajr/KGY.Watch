9fd33e6b665269e4deac13ecd2510a2e614a40cf
gyula.krajczar <gyula.krajczar@gmail.com>
2017-09-20 17:30:27 +0200

    Initial commit (speghetti mmmm)

--- KGY.Watch/App.xaml.cs
+++ KGY.Watch/App.xaml.cs
@@ -1,17 +1,42 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Drawing;
 using System.Windows;
+using System.Windows.Forms;
 
 namespace KGY.Watch
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App : System.Windows.Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var bitmap = new Bitmap(GetResourceStream(new System.Uri("pack://application:,,,/clock.png")).Stream);
+            var iconHandle = bitmap.GetHicon();
+
+            var contextMenu = new ContextMenu();
+            contextMenu.MenuItems.Add(new MenuItem("Properties", PropertiesMenuItem_Click));
+            contextMenu.MenuItems.Add(new MenuItem("-"));
+            contextMenu.MenuItems.Add(new MenuItem("Exit", ExitMenuItem_Click));
+
+            NotifyIcon ni = new NotifyIcon
+            {
+                ContextMenu = contextMenu,
+                Icon = Icon.FromHandle(iconHandle),
+                Visible = true
+            };
+        }
+
+        private void ExitMenuItem_Click(object sender, EventArgs e)
+        {
+            Shutdown();
+        }
+
+        private void PropertiesMenuItem_Click(object sender, EventArgs e)
+        {
+        }
     }
 }
--- KGY.Watch/MainWindow.xaml.cs
+++ KGY.Watch/MainWindow.xaml.cs
@@ -1,16 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace KGY.Watch
@@ -20,9 +13,90 @@ namespace KGY.Watch
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Timer timer = new Timer();
+        private Timer conwayTimer = new Timer();
+        private Conway conway;
+
         public MainWindow()
         {
+            timer.Interval = 1000;
+            timer.Elapsed += Timer_Elapsed;
+
+            conwayTimer.Interval = 300;
+            conwayTimer.Elapsed += ConwayTimer_Elapsed;
+
             InitializeComponent();
+
+            Timer_Elapsed(null, null);
+
+            timer.Start();
+        }
+
+        private void ConwayTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                conway.Turn();
+                ConwayCanvas.Children.Clear();
+
+                int trueCount = 0;
+                for (var x = 0; x < conway.Width; ++x)
+                {
+                    for (var y = 0; y < conway.Height; ++y)
+                    {
+                        if (conway.Cells[x, y])
+                        {
+                            var cellRect = new Rectangle();
+                            cellRect.Fill = new SolidColorBrush(Colors.Green);
+                            cellRect.Width = 3;
+                            cellRect.Height = 3;
+                            Canvas.SetLeft(cellRect, x * 3);
+                            Canvas.SetTop(cellRect, y * 3);
+
+                            ConwayCanvas.Children.Add(cellRect);
+                            trueCount++;
+                        }
+                    }
+                }
+                ConwayCanvas.InvalidateVisual();
+            });
+        }
+
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                txtTime.Text = $"{DateTime.Now.ToShortTimeString()}";
+            });
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                DragMove();
+            }
+        }
+
+        private void Window_Unloaded(object sender, RoutedEventArgs e)
+        {
+            timer.Stop();
+            conwayTimer.Stop();
+        }
+
+        private void ExitMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            int width = (int)Math.Floor(ConwayCanvas.ActualWidth / 3);
+            int height = (int)Math.Floor(ConwayCanvas.ActualHeight / 3);
+            conway = new Conway(width, height);
+            conway.RandomSeed();
+
+            conwayTimer.Start();
         }
     }
 }
